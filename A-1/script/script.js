156827dbf632c66d0204eaba92066bb0bafded48
홍동화 <hhhhong44@gmail.com>
2023-03-23 14:11:02 +0900

    A-1 finish

--- A-1/script/script.js
+++ A-1/script/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  //**** 마우스오버  ****//
   //서브메뉴 숨기고 시작
   $(".subMenu").hide();
   // 마우스 오버
@@ -10,4 +9,32 @@ $(document).ready(function () {
     .mouseout(function () {
       $(".subMenu").stop().slideUp(300);
     });
+});
+
+// 이미지 슬라이드
+
+setInterval(function () {
+  $(".slideWrap").animate({ "margin-top": "-300" }, function () {
+    $(".slide:first").appendTo(".slideWrap");
+    $(".slideWrap").css({ "margin-top": "0" });
+  });
+}, 3000);
+
+$(".tabMenu li").click(function () {
+  var idx = $(this).index();
+
+  $(".tabItem >*").hide().removeClass("on");
+  $(".tabItem >*").eq(idx).show().addClass("on");
+
+  $(".tabMenu li").removeClass("on");
+  $(this).addClass("on");
+});
+
+//popUpBox---------------------------------------------------------
+$(".notice li:nth-child(1)").click(function () {
+  $(".popUpBox").show();
+});
+
+$(".popUpBox button").click(function () {
+  $(".popUpBox").hide();
 });
