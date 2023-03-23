2a22df5c03c81ec2cabc64dfa9b9af21255524c3
홍동화 <hhhhong44@gmail.com>
2023-03-23 18:26:43 +0900

    [feat] menu_vertical3_ing

--- practice/header_menu/세로형3/script/script.js
+++
@@ -1,7 +0,0 @@
-$("header nav >ul").mouseenter(function () {
-  $("ul.lv2, .headerBg").stop().slideDown();
-});
-
-$("header nav >ul").mouseleave(function () {
-  $("ul.lv2, .headerBg").stop().slideUp();
-});
